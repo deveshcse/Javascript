f8cdf28e05b07eecc63d494836864fda8f7a9813
deveshcse <d.mishra56603@gmail.com>
2024-08-24 13:07:46 +0530

    added scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,4 +39,35 @@ function loginUserMessage(user = "Default user") {
 
 // console.log(loginUserMessage("devesh"))
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(100, 200, 300, 5000, 8000));
+
+const user = {
+    username: "devesh",
+    price: 199
+}
+
+function handleObject(anObject) {
+    console.log(`Username is ${anObject.username} and price is ${anObject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "devesh",
+    price: 199
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(anArray){
+    return anArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+// var c = 300
+
+let a = 300
+
+if (true){
+    let a = 10
+    const b = 20
+    console.log("Inner:", a);
+
+    
+}
+
+console.log("Outer:",a);
+
+
+function one(){
+    const username = "devesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+
+if (true){
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+//-------------------interesting------------------
+
+addone(5)
+function addone(num){
+    return num+1
+}
+
+
+
+
+addTwo(5)   
+const addTwo = function(num){
+    return num+2
+}
