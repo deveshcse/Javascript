e03bf573157a7ab4e95275259d0dad9d1f83879b
deveshcse <d.mishra56603@gmail.com>
2024-08-24 15:48:12 +0530

    added IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB connected`);
+    
+})();
+
+( (name) => {
+    console.log(`DB connected two ${name}`);
+    
+})("Devesh");
